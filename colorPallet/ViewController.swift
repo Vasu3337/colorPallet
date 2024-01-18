013645113e864b6025a73abec4d544ce8d721585
mahesh <89373680+Vasu3337@users.noreply.github.com>
2024-01-18 14:54:01 +0530

    sdfergegvfdvsfvdfv

--- colorPallet/ViewController.swift
+++ colorPallet/ViewController.swift
@@ -78,6 +78,8 @@ extension GridViewController {
 }
 //sdhghjd
 
+
+
 extension GridViewController {
     private func configureHierarchy() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
